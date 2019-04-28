0ecab4768d4a7611fc7b19df9d9194072c8e746a
Tdogb <tdogbeard@gmail.com>
2019-04-28 19:45:55 -0400

    Working motion profile velocity

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Arrays;
 
 public class Main {
@@ -8,15 +9,17 @@ public class Main {
     final double robotWheelBase = 0.18; //Diameter
     final double robotWheelDiameter = 0.051;
 
-    double cruiseStart = 1;
+    double cruiseStart = 100;
     double cruiseEnd = 0;
+    double cruiseTime = 0;
+    double pathTime = 0;
     boolean instantaneousCruise = false;
     boolean velocityProfileGenerated = false;
     double tconversionFactor = 1;
 
     public Main() {
         p0 = new Vector(0,0);
-        p1 = new Vector(5,7);
+        p1 = new Vector(2,4);
         t0 = new Vector(1,0);
         t1 = new Vector(1,0);
         generateSpline();
@@ -25,25 +28,30 @@ public class Main {
     public double deltaTheta(double t) {
         //Derrivative of atan2(x'/y')
         //Change in velocity = vdiff
-        return (3*a.y()*Math.pow(t,2)+2*b.y()*t+c.y())/(Math.pow(3*a.x()*Math.pow(t,2)+2*b.x()*t+c.x(),2) + Math.pow(3*a.y()*Math.pow(t,2)+2*b.y()*t+c.y(),2));
+        //return (3*a.y()*Math.pow(t,2)+2*b.y()*t+c.y())/(Math.pow(3*a.x()*Math.pow(t,2)+2*b.x()*t+c.x(),2) + Math.pow(3*a.y()*Math.pow(t,2)+2*b.y()*t+c.y(),2));
+        double prevAngle = Math.atan((3*a.y()*Math.pow(t-timeStep,2)+2*b.y()*(t-1)+c.y())/(3*a.x()*Math.pow(t-timeStep,2)+2*b.x()*(t-timeStep)+c.x()));
+        double angle = Math.atan((3*a.y()*Math.pow(t,2)+2*b.y()*t+c.y())/(3*a.x()*Math.pow(t,2)+2*b.x()*t+c.x()));
+        return angle-prevAngle;
     }
 
     /**
      * velocityProfile
-     * @return Base velocity setpoint at position
+     * @return Base velocity setpoint at time
      */
-    public double velocityProfile(double _t) {
-        double t;
+    public double velocityProfile(double t) {
+//        double t;
         if(velocityProfileGenerated) {
-            t = _t * tconversionFactor;
+//            t = _t * tconversionFactor;
             if(t < cruiseStart) {
-                return 0.5*maxAccel*Math.pow(t,2);
+//                return 0.5*maxAccel*Math.pow(t,2);
+                return maxAccel*t;
             }
             else if(t>= cruiseStart && t<= cruiseEnd) {
                 return maxVel;
             }
             else {
-                return 0.5*maxAccel*Math.pow(1-t, 2);
+//                return maxVel - 0.5*maxAccel*Math.pow((t-cruiseEnd), 2);
+                return maxVel - maxAccel*(t-cruiseEnd);
             }
         }
         else {
@@ -68,11 +76,11 @@ public class Main {
         double v = 0;
         double distance = 0;
         double t = timeStep;
-        //Acceleration
-        while(v < maxVel && t < cruiseStart) {
+        //Acceleration IN REAL TIME UNITS
+        while(v < maxVel && !instantaneousCruise) {
             double vi = v;
             v = v + maxAccel * timeStep;
-            distance += (v-vi)/timeStep;
+            distance += (v-vi)*timeStep;
             if(distance > arcLength/2) {
                 cruiseStart = t;
                 cruiseEnd = t;
@@ -80,7 +88,9 @@ public class Main {
             }
             t+=timeStep;
             totalTime += timeStep;
-            System.out.println(v);
+//            System.out.println("");
+//            System.out.print(distance);
+//            System.out.print("  " + v);
             if(v >= maxVel) {
                 v = maxVel;
                 cruiseStart = t;
@@ -89,28 +99,32 @@ public class Main {
         }
         //Deceleration
         if(!instantaneousCruise) {
-            t = 1;
-            distance = arcLength;
+            cruiseEnd = cruiseStart;
+            t = 0;
+            distance = 0;
             v = 0;
-            while(v < maxVel && t > cruiseEnd) {
+            while(v < maxVel) {
                 double vi = v;
-                v = (Math.pow(v,2) + maxAccel*timeStep);
-                distance += (v-vi)/-timeStep; //Adding negative distance to move backwards
+                v = v + maxAccel*timeStep;
+                distance += (v-vi)*timeStep;
                 if(v >= maxVel) {
-                    cruiseEnd = t;
+                    cruiseTime = (arcLength - 2 * distance)/maxVel;
+                    cruiseEnd = cruiseStart + cruiseTime;
                 }
-                t -= timeStep;
+                t += timeStep;
                 totalTime += timeStep;
             }
             //totalTime = (1 - (cruiseEnd-cruiseStart)); in terms of ratios.
             //T total time is the 2 triangles in the trapezoid, and the cruise is the block in the middle
             //Divided by total time because it is a conversion
-            tconversionFactor = (1-(cruiseEnd-cruiseStart))/(totalTime);
+//            tconversionFactor = (1-(cruiseEnd-cruiseStart))/(totalTime);
         }
         else {
             totalTime *= 2;
-            tconversionFactor = totalTime;
+//            tconversionFactor = totalTime;
         }
+        pathTime = cruiseEnd + cruiseStart; //Because cruisestart should be one of the triangles and cruiseend is one triangle + middle
+        tconversionFactor = 1/pathTime;
         velocityProfileGenerated = true;
     }
 
@@ -130,17 +144,19 @@ public class Main {
         c = new Vector(cx,cy);
         d = new Vector(dx,dy);
         generateVelocityProfile();
-        for(double t = 0; t <= 1; t+=timeStep) {
+        debug();
+        for(double time = 0; time <= pathTime; time+=timeStep) {
+            double t = time * tconversionFactor;
             System.out.println("");
-            System.out.print(t);
+            System.out.print(time);
             System.out.print(",");
             System.out.print(a.x()*Math.pow(t,3)+b.x()*Math.pow(t,2)+c.x()*t+d.x());
             System.out.print(",");
             System.out.print(a.y()*Math.pow(t,3)+b.y()*Math.pow(t,2)+c.y()*t+d.y());
             System.out.print(",");
-            System.out.print(velocityProfile(t));
+            System.out.print(velocityProfile(time));
             System.out.print(",");
-            System.out.print(deltaTheta(t));
+            System.out.print(deltaTheta(time));
             System.out.print(",");
         }
         System.out.println("");
@@ -151,6 +167,13 @@ public class Main {
 
         System.out.println(cruiseStart);
         System.out.println(cruiseEnd);
+        System.out.println(instantaneousCruise);
+    }
+
+    public void debug() {
+        for (double time = 0; time <= pathTime; time += timeStep) {
+            System.out.println(deltaTheta(time));
+        }
     }
 
     public static void main(String[] args)
