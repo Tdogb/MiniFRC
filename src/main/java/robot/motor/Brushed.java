dbd5ce9e7dc58f0ad902a7c1d6cc781695724766
Tdogb <tdogbeard@gmail.com>
2019-05-02 18:00:36 -0400

    Robot functionality

--- src/main/java/robot/motor/Brushed.java
+++ src/main/java/robot/motor/Brushed.java
@@ -1,23 +1,21 @@
 package robot.motor;
 
-
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
 import com.pi4j.io.gpio.Pin;
 import com.pi4j.io.gpio.PinState;
 
 public class Brushed {
-    GpioPinDigitalOutput forwardPin;
-    GpioPinDigitalOutput reversePin;
-
-    public Brushed(Pin fwdPin, Pin rvsPin) {
-        forwardPin = robot.Global.gpio.provisionDigitalOutputPin(fwdPin, PinState.LOW);
-        reversePin = robot.Global.gpio.provisionDigitalOutputPin(rvsPin, PinState.LOW);
-        forwardPin.setShutdownOptions(true);
-        reversePin.setShutdownOptions(true);
-
-    }
-
-    public void drive(boolean dir, int voltage) {
-
-    }
+//    GpioPinDigitalOutput forwardPin;
+//    GpioPinDigitalOutput reversePin;
+//
+//    public Brushed(Pin fwdPin, Pin rvsPin) {
+//        forwardPin = robot.Global.gpio.provisionDigitalOutputPin(fwdPin, PinState.LOW);
+//        reversePin = robot.Global.gpio.provisionDigitalOutputPin(rvsPin, PinState.LOW);
+//        forwardPin.setShutdownOptions(true);
+//        reversePin.setShutdownOptions(true);
+//    }
+//
+//    public void drive(boolean dir, int voltage) {
+//
+//    }
 }
