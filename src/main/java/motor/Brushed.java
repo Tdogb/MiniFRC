33998f0ef0df62679bf7c2adb60af0fae172b33e
Tdogb <tdogbeard@gmail.com>
2019-05-01 20:55:35 -0400

    Maven update

--- src/main/java/motor/Brushed.java
+++ src/main/java/motor/Brushed.java
@@ -1,4 +1,15 @@
 package motor;
 
 public class Brushed {
+    int forwardPin;
+    int reversePin;
+
+    public Brushed(int forwardPin, int reversePin) {
+        this.forwardPin = forwardPin;
+        this.reversePin = reversePin;
+    }
+
+    public void drive(boolean dir, int voltage) {
+
+    }
 }
